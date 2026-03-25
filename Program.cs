1306a5a526eeeed3bd6109eed45fcf54a1a9a5fb
ViceElez <vice.elezovic12@gmail.com>
2026-03-25 19:09:48 +0100

    add more words

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("nesto novo")
+Console.WriteLine("nesto novo");
+Console.WriteLine("jos nesto novo");
